c6f833bc3e224eb08b8dabd5db4102911cfb7878
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-09-17 18:49:42 -0300

    Added impl on input for doing masked inputs (passwords)

--- internal/ui/components/input.go
+++ internal/ui/components/input.go
@@ -5,10 +5,14 @@ import (
 	"github.com/rivo/tview"
 )
 
-func NewInput(label string, width int, color tcell.Color, background tcell.Color) *tview.InputField {
+func NewInput(label string, width int, color tcell.Color, background tcell.Color, mask bool) *tview.InputField {
 	input := tview.NewInputField().
 		SetLabel(label).
 		SetFieldWidth(width)
+
+	if mask {
+		input.SetMaskCharacter('*')
+	}
 
 	addColors(input, color)
 
