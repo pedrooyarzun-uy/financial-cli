8424cf7609f21aabe1f879cf963466a941b81c99
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-09-27 01:05:05 -0300

    Better style for warning modal

--- internal/ui/components/warning_modal.go
+++ internal/ui/components/warning_modal.go
@@ -1,15 +1,22 @@
 package components
 
-import "github.com/rivo/tview"
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
 
 func NewWarningModal(text string, pages *tview.Pages) *tview.Modal {
 	modal := tview.NewModal().
-		SetText(text).
+		SetBackgroundColor(tcell.ColorYellow).
+		SetTextColor(tcell.ColorBlack).
+		SetText("\u26A0 " + text + " \u26A0").
 		AddButtons([]string{"OK"}).
 		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
 			pages.RemovePage("modal")
 		})
 
+	modal.SetTitle("Warning")
+	modal.SetTitleColor(tcell.ColorYellow)
 	return modal
 
 }
