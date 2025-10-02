acf870b08c4a6deff7d253bf72020f02fa5ffb22
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-10-02 20:10:13 -0300

    feature: Added email validator

---
+++ internal/ui/validators/email.go
@@ -0,0 +1,9 @@
+package validators
+
+import "regexp"
+
+func CheckEmail(email string) bool {
+
+	re := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
+	return re.MatchString(email)
+}
