3854f5148f755adb0492dbc77e712aa708fa7f7a
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-10-01 23:42:55 -0300

    feat: added custom modal for creating own categories

--- internal/ui/pages/home/options/category.go
+++ internal/ui/pages/home/options/category.go
@@ -1,7 +1,31 @@
 package options
 
-import "fmt"
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/pedrooyarzun-uy/financial-cli/internal/ui/components"
+	"github.com/rivo/tview"
+)
 
-func Main() {
-	fmt.Println("Hola mundo")
+func NewCategory(pages *tview.Pages) tview.Primitive {
+
+	form := tview.NewForm().
+		AddInputField("Name:", "", 30, nil, nil).
+		SetLabelColor(tcell.ColorCornflowerBlue).
+		SetFieldBackgroundColor(tcell.ColorDarkSlateGrey).
+		AddButton("OK", func() {
+			pages.RemovePage("category")
+		}).
+		AddButton("Cancel", func() {
+			pages.RemovePage("category")
+		})
+
+	form.SetBorder(true).
+		SetTitle("NEW CATEGORY").
+		SetTitleColor(tcell.ColorYellow).
+		SetTitleAlign(tview.AlignCenter).
+		SetBackgroundColor(tcell.ColorLightSlateGray)
+
+	modal := components.NewFormModal(pages, form)
+
+	return modal
 }
