398256e8b9e8376f6494f0a0e2d2957175d30eae
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2025-10-02 20:11:49 -0300

    feat: Added sign up page

---
+++ internal/ui/pages/sign_up.go
@@ -0,0 +1,61 @@
+package pages
+
+import (
+	"fmt"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/pedrooyarzun-uy/financial-cli/internal/ui/components"
+	"github.com/pedrooyarzun-uy/financial-cli/internal/ui/validators"
+	"github.com/rivo/tview"
+)
+
+func NewSignUp(pages *tview.Pages) *tview.Flex {
+
+	form := tview.NewForm()
+
+	//Styles for form
+	form.SetFieldBackgroundColor(tcell.ColorPaleVioletRed)
+	form.SetLabelColor(tcell.ColorWhite)
+	form.SetButtonBackgroundColor(tcell.ColorPaleVioletRed)
+
+	//Content
+	form.AddInputField("Name:", "", 30, nil, nil)
+	form.AddInputField("Email:", "", 30, nil, nil)
+	form.AddPasswordField("Password:", "", 30, '*', nil)
+	form.AddPasswordField("Repeat Password:", "", 30, '*', nil)
+
+	form.AddButton("Sign Up", func() {
+
+		//Get values
+		name := form.GetFormItem(0).(*tview.InputField).GetText()
+		email := form.GetFormItem(1).(*tview.InputField).GetText()
+		password := form.GetFormItem(2).(*tview.InputField).GetText()
+		repeatedPasssword := form.GetFormItem(3).(*tview.InputField).GetText()
+
+		if password != repeatedPasssword {
+			modal := components.NewWarningModal("Passwords do not match", pages)
+			pages.AddPage("modal", modal, true, true)
+			return
+		}
+
+		if !validators.CheckEmail(email) {
+			modal := components.NewWarningModal("Email is not valid.", pages)
+			pages.AddPage("modal", modal, true, true)
+			return
+		}
+
+		fmt.Println(name, email, password)
+	})
+
+	flex := tview.NewFlex()
+	flex.SetDirection(tview.FlexColumn)
+	flex.SetBorder(true).
+		SetTitle("SIGN UP").
+		SetBorderColor(tcell.ColorPaleVioletRed).
+		SetTitleAlign(0).
+		SetTitleColor(tcell.ColorPaleVioletRed)
+	flex.AddItem(form, 30, 1, true)
+
+	return flex
+
+}
