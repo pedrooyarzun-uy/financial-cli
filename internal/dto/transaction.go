b8ccbde2c221803062de2a6366e5751b1e6bc309
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-01-04 18:33:36 -0300

    feature: add new transaction service and dto for allowing sending transactions to backend

---
+++ internal/dto/transaction.go
@@ -0,0 +1,14 @@
+package dto
+
+type AddTransactionReq struct {
+	Amount   float64 `json:"amount" binding:"required"`
+	Account  int     `json:"account" binding:"required"`
+	Currency int     `json:"currency" binding:"required"`
+	Type     int     `json:"type" binding:"required"`
+	Category int     `json:"category"`
+	Notes    string  `json:"notes" `
+}
+
+type AddTransactionRes struct {
+	Message string `json:"message" binding:"required"`
+}
