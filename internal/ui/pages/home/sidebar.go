98f22c5b663c30e78b8c47438cdd8fd281755375
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-01-06 16:13:59 -0300

    feature: added sidebar.go page

---
+++ internal/ui/pages/home/sidebar.go
@@ -0,0 +1,38 @@
+package home
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pedrooyarzun-uy/financial-cli/internal/api"
+	"github.com/pedrooyarzun-uy/financial-cli/internal/services"
+	"github.com/rivo/tview"
+)
+
+func NewSidebar(app *tview.Application, pages *tview.Pages) *tview.Flex {
+
+	ts := services.NewTransactionService(api.CLIENT)
+
+	sidebar := tview.NewFlex()
+
+	categories, err := ts.GetTotalsByCategory()
+
+	list := tview.NewTextView().SetDynamicColors(true)
+
+	if err != nil {
+		list.SetText(err.Error())
+		sidebar.AddItem(list, 0, 1, true)
+		return sidebar
+	}
+
+	var b strings.Builder
+	for _, c := range categories {
+		fmt.Fprintf(&b, "[#00ffcc]%s[-]: $%.2f\n", c.Category, c.Total)
+	}
+
+	list.SetText(b.String())
+
+	sidebar.AddItem(list, 0, 1, true)
+
+	return sidebar
+}
