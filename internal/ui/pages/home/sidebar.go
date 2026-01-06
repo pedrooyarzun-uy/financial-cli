9c2b4cfabdd294b32ad3127d2f5c80a39f68f3bd
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-01-06 19:01:18 -0300

    feature: added impl of graphic generation for displaying expenses on categories

--- internal/ui/pages/home/sidebar.go
+++ internal/ui/pages/home/sidebar.go
@@ -2,8 +2,11 @@ package home
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 
+	"github.com/gdamore/tcell/v2"
+	"github.com/navidys/tvxwidgets"
 	"github.com/pedrooyarzun-uy/financial-cli/internal/api"
 	"github.com/pedrooyarzun-uy/financial-cli/internal/services"
 	"github.com/rivo/tview"
@@ -12,12 +15,15 @@ import (
 func NewSidebar(app *tview.Application, pages *tview.Pages) *tview.Flex {
 
 	ts := services.NewTransactionService(api.CLIENT)
-
-	sidebar := tview.NewFlex()
-
+	sidebar := tview.NewFlex().SetDirection(tview.FlexRow)
 	categories, err := ts.GetTotalsByCategory()
 
+	//Graphic items
 	list := tview.NewTextView().SetDynamicColors(true)
+	bc := tvxwidgets.NewBarChart()
+
+	bc.SetTitle("Gastos por categoría")
+	bc.SetBorder(false)
 
 	if err != nil {
 		list.SetText(err.Error())
@@ -28,11 +34,17 @@ func NewSidebar(app *tview.Application, pages *tview.Pages) *tview.Flex {
 	var b strings.Builder
 	for _, c := range categories {
 		fmt.Fprintf(&b, "[%s]%s[-]: $%.2f\n", c.Color, c.Category, c.Total)
+
+		c.Color = strings.TrimPrefix(c.Color, "#")
+		v, _ := strconv.ParseInt(c.Color, 16, 32)
+
+		bc.AddBar(c.Category[:7], int(c.Total), tcell.NewHexColor(int32(v)))
 	}
 
 	list.SetText(b.String())
 
 	sidebar.AddItem(list, 0, 1, true)
+	sidebar.AddItem(bc, 0, 1, true)
 
 	return sidebar
 }
