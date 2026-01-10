a6fece9da994ee314c12e4633f1f6ded13b8a1d8
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-01-10 16:38:09 -0300

    feature: added switch to new transactions_view page

--- internal/ui/pages/home/main.go
+++ internal/ui/pages/home/main.go
@@ -15,7 +15,9 @@ func NewMain(app *tview.Application, pages *tview.Pages) *tview.Flex {
 		}).
 		AddItem("Automatic expense entry", "", 's', nil).
 		AddItem("Add account", "", 'd', nil).
-		AddItem("View stats", "", 'f', nil).
+		AddItem("View stats", "", 'f', func() {
+			pages.SwitchToPage("transactions_view")
+		}).
 		AddItem("Settings", "", 'g', nil).
 		AddItem("Quit", "", 'q', func() {
 			app.Stop()
