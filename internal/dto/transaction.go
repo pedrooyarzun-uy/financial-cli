87f9589973494de7465983c26f041109db847135
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-01-11 12:59:28 -0300

    feature: added new transactions by detail dtos

--- internal/dto/transaction.go
+++ internal/dto/transaction.go
@@ -1,5 +1,7 @@
 package dto
 
+import "time"
+
 type AddTransactionReq struct {
 	Amount   float64 `json:"amount" binding:"required"`
 	Account  int     `json:"account" binding:"required"`
@@ -22,4 +24,19 @@ type TotalByCategory struct {
 type TotalsByCategoryRes struct {
 	Message string            `json:"message"`
 	Totals  []TotalByCategory `json:"totals"`
+}
+
+type TransactionByDetail struct {
+	Id          int       `db:"id"`
+	Category    string    `db:"category"`
+	Subcategory string    `db:"subcategory"`
+	Amount      float64   `db:"amount"`
+	Currency    string    `db:"currency"`
+	Notes       string    `db:"notes"`
+	Date        time.Time `db:"created_at"`
+}
+
+type GetTransactionsByDetailRes struct {
+	Message      string                `json:"message"`
+	Transactions []TransactionByDetail `json:"transactions"`
 }
