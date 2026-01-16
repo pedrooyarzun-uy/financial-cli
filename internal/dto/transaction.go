b5d041452c0ae7381705fa32e9e7bea344e34c35
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-01-16 00:26:39 -0300

    feature: added color on transaction

--- internal/dto/transaction.go
+++ internal/dto/transaction.go
@@ -35,6 +35,7 @@ type TransactionByDetail struct {
 	Notes       string    `db:"notes"`
 	Date        time.Time `db:"created_at"`
 	Color       string    `db:"color"`
+	Type        int       `db:"type"`
 }
 
 type GetTransactionsByDetailRes struct {
--- internal/ui/pages/home/transaction_view/table.go
+++ internal/ui/pages/home/transaction_view/table.go
@@ -21,7 +21,6 @@ func NewTransactionsTable(transactions []dto.TransactionByDetail) *tview.Table {
 	table.SetCell(0, 5, tview.NewTableCell("Date").SetSelectable(false).SetAttributes(tcell.AttrBold).SetTextColor(tcell.ColorYellow).SetAlign(tview.AlignCenter))
 
 	for idx, val := range transactions {
-
 		//Parsing color for tcell
 		val.Color = strings.TrimPrefix(val.Color, "#")
 		v, _ := strconv.ParseInt(val.Color, 16, 32)
@@ -29,7 +28,13 @@ func NewTransactionsTable(transactions []dto.TransactionByDetail) *tview.Table {
 		idx += 1
 		table.SetCell(idx, 0, tview.NewTableCell(val.Category).SetTextColor(tcell.NewHexColor(int32(v))))
 		table.SetCell(idx, 1, tview.NewTableCell(val.Subcategory))
-		table.SetCell(idx, 2, tview.NewTableCell(strconv.FormatFloat(val.Amount, 'f', 2, 64)))
+
+		if val.Type == 1 {
+			table.SetCell(idx, 2, tview.NewTableCell(strconv.FormatFloat(val.Amount, 'f', 2, 64)).SetTextColor(tcell.ColorGreen))
+		} else if val.Type == 2 {
+			table.SetCell(idx, 2, tview.NewTableCell(strconv.FormatFloat(val.Amount, 'f', 2, 64)).SetTextColor(tcell.ColorRed))
+		}
+
 		table.SetCell(idx, 3, tview.NewTableCell(val.Currency))
 		table.SetCell(idx, 4, tview.NewTableCell(val.Notes))
 		table.SetCell(idx, 5, tview.NewTableCell(val.Date.String()))
