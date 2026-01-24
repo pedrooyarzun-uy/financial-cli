a0ea069ec00889f69ee4482205da16dddb0a3d4e
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-01-24 02:11:26 -0300

    feature: added new styles for dropdown adding consistency on interface

--- internal/ui/components/dropdown.go
+++ internal/ui/components/dropdown.go
@@ -1,13 +1,25 @@
 package components
 
-import "github.com/rivo/tview"
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
 
 func NewDropDown(label string, labelWidth int, fieldWidth int, options []string, selectedFunc func(text string, index int)) *tview.DropDown {
 	dropdown := tview.NewDropDown().
 		SetLabel(label).
 		SetLabelWidth(labelWidth).
 		SetFieldWidth(fieldWidth).
-		SetOptions(options, selectedFunc)
+		SetOptions(options, selectedFunc).
+		SetFieldBackgroundColor(tcell.ColorGray).
+		SetFieldTextColor(tcell.ColorWhite).
+		SetLabelColor(tcell.ColorWhite).
+		SetListStyles(
+			tcell.StyleDefault.Background(tcell.ColorWhite).Foreground(tcell.ColorBlack),
+			tcell.StyleDefault.Background(tcell.ColorGray).Foreground(tcell.ColorWhite),
+		).
+		SetPrefixStyle(tcell.StyleDefault.Foreground(tcell.ColorGray).Foreground(tcell.ColorWhite)).
+		SetFocusedStyle(tcell.StyleDefault.Background(tcell.ColorWhite).Foreground(tcell.ColorBlack))
 
 	return dropdown
 }
