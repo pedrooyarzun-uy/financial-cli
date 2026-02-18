86989e42f0a246f27873212aa0052f79c36ae1e3
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-02-17 21:27:51 -0300

    feature: new chart component with color support

---
+++ internal/ui/components/chart.go
@@ -0,0 +1,31 @@
+package components
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/navidys/tvxwidgets"
+)
+
+func NewChart(title string) *tvxwidgets.BarChart {
+	chart := tvxwidgets.NewBarChart()
+	chart.SetTitle(title)
+	chart.SetBorder(true)
+
+	return chart
+}
+
+func AddItem(chart *tvxwidgets.BarChart, label string, value int, color string) {
+	var b strings.Builder
+	fmt.Fprintf(&b, "[%s]%s[-]: $%d\n", parseHexColor(color), label, value)
+	chart.AddBar(label[:7], value, parseHexColor(color))
+}
+
+func parseHexColor(hex string) tcell.Color {
+	color := strings.TrimPrefix(hex, "#")
+	v, _ := strconv.ParseInt(color, 16, 32)
+
+	return tcell.NewHexColor(int32(v))
+}
