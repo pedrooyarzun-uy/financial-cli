36d1596addfd89ac44bb77b9b7fe4c1606b13ec1
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-03-27 10:18:33 -0300

    feature: new type transaction dropdown

---
+++ internal/ui/components/dropdowns/type_dropdown.go
@@ -0,0 +1,30 @@
+package dropdowns
+
+import "github.com/pedrooyarzun-uy/financial-cli/internal/ui/components"
+
+type TypeDropdown struct {
+	*components.DropDown
+	types      map[string]int
+	onSelected func(typeId int)
+}
+
+func NewTypeDropdown(label string, labelWidth int, fieldWidth int) *TypeDropdown {
+	d := components.NewDropDown(label, labelWidth, fieldWidth, []string{}, nil)
+
+	component := TypeDropdown{
+		DropDown: d,
+		types: map[string]int{
+			"Income":  1,
+			"Outcome": 2,
+		},
+	}
+
+	return &component
+
+}
+
+func (d *TypeDropdown) GetSelectedTypeID() (int, bool) {
+	_, label := d.GetCurrentOption()
+	typeID, ok := d.types[label]
+	return typeID, ok
+}
