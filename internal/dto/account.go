00196f74d113aeb44a71c80b75e5528fdebed1ca
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-03-29 17:06:49 -0300

    feature: added add transaction req struct

--- internal/dto/account.go
+++ internal/dto/account.go
@@ -6,3 +6,10 @@ type GetAllAcounts struct {
 	Message  string `json:"message"`
 	Accounts []domain.Account
 }
+
+type AddAccountReq struct {
+	Name     string `json:"name"`
+	Number   string `json:"number"`
+	Currency int    `json:"currency"`
+	Bank     int    `json:"bank"`
+}
