fb3d7526d5e58eef4fcbd063fe2987b14418ba10
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-03-30 22:06:37 -0300

    feeature: added add page

---
+++ internal/ui/pages/home/options/add/add.go
@@ -0,0 +1,26 @@
+package add
+
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+
+func NewAdd(app *tview.Application, pages *tview.Pages) *tview.Flex {
+	pages.AddPage("add_transaction", NewTransaction(app, pages), true, true)
+	pages.AddPage("add_account", NewAddAccount(app, pages), true, true)
+
+	main := tview.NewFlex()
+
+	list := tview.NewList().
+		AddItem("Add expense", "", 'e', func() {
+			pages.SwitchToPage("add_transaction")
+		}).
+		AddItem("Add account", "", 'e', func() {
+			pages.SwitchToPage("add_account")
+		})
+
+	list.SetShortcutColor(tcell.ColorOrange)
+	main.AddItem(list, 0, 1, true)
+
+	return main
+}
