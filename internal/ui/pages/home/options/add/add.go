d089829d67a262a00933a20a5ad4818a63e9d629
Pedro Oyarzun <pedrooyarzun2015@gmail.com>
2026-04-08 16:05:53 -0300

    feature: added new option on add menu

--- internal/ui/pages/home/options/add/add.go
+++ internal/ui/pages/home/options/add/add.go
@@ -8,6 +8,7 @@ import (
 func NewAdd(app *tview.Application, pages *tview.Pages) *tview.Flex {
 	pages.AddPage("add_transaction", NewTransaction(app, pages), true, true)
 	pages.AddPage("add_account", NewAddAccount(app, pages), true, true)
+	pages.AddPage("add_credit_card", NewAddCreditCard(app, pages), true, true)
 
 	main := tview.NewFlex()
 
@@ -17,6 +18,9 @@ func NewAdd(app *tview.Application, pages *tview.Pages) *tview.Flex {
 		}).
 		AddItem("Add account", "", 's', func() {
 			pages.SwitchToPage("add_account")
+		}).
+		AddItem("Add credit card", "", 'd', func() {
+			pages.SwitchToPage("add_credit_card")
 		})
 
 	list.SetShortcutColor(tcell.ColorOrange)
